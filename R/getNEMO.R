322a48ac516c9e056582f895ce6e9ead0819b400
xlucpu <xlu.cpu@foxmail.com>
2020-09-02 15:02:11 +0800

    lastest files

--- R/getNEMO.R
+++ R/getNEMO.R
@@ -1,3 +1,49 @@
+#' @title affinityMatrix
+#' @name affinityMatrix
+#' @param Diff Distance Matrix.
+#' @param K Number of nearest neighbors.
+#' @param sigma Variance for local model.
+#' @author Bo Wang, Aziz Mezlini, Feyyaz Demir, Marc Fiume, Zhuowen Tu, Michael Brudno, Benjamin Haibe-Kains, Anna Goldenberg
+#' @keywords internal
+affinityMatrix <- function(Diff,K=20,sigma=0.5) {
+  ###This function constructs similarity networks.
+  N = nrow(Diff)
+
+  Diff = (Diff + t(Diff)) / 2
+  diag(Diff) = 0;
+  sortedColumns = as.matrix(t(apply(Diff,2,sort)))
+  finiteMean <- function(x) { mean(x[is.finite(x)]) }
+  means = apply(sortedColumns[,1:K+1],1,finiteMean)+.Machine$double.eps;
+
+  avg <- function(x,y) ((x+y)/2)
+  Sig = outer(means,means,avg)/3*2 + Diff/3 + .Machine$double.eps;
+  Sig[Sig <= .Machine$double.eps] = .Machine$double.eps
+  densities = dnorm(Diff,0,sigma*Sig,log = FALSE)
+
+  W = (densities + t(densities)) / 2
+  return(W)
+}
+
+#' @title dist2
+#' @name dist2
+#' @param X A data matrix where each row is a different data point.
+#' @param C A data matrix where each row is a different data point. If this matrix is the same as X, pairwise distances for all data points are computed.
+#' @author Bo Wang, Aziz Mezlini, Feyyaz Demir, Marc Fiume, Zhuowen Tu, Michael Brudno, Benjamin Haibe-Kains, Anna Goldenberg
+#' @keywords internal
+dist2 <- function(X,C) {
+  ndata = nrow(X)
+  ncentres = nrow(C)
+
+  sumsqX = rowSums(X^2)
+  sumsqC = rowSums(C^2)
+
+  XC = 2 * (X %*% t(C))
+
+  res = matrix(rep(sumsqX,times=ncentres),ndata,ncentres) + t(matrix(rep(sumsqC,times=ndata),ncentres,ndata)) - XC
+  res[res < 0] = 0
+  return(res)
+}
+
 #' @name getNEMO
 #' @title Get subtypes from NEMO
 #' @description This function wraps the NEMO (Neighborhood based multi-omics clustering) algorithm and provides standard output for `getMoHeatmap()` and `getConsensusMOIC()`.
@@ -13,7 +59,6 @@
 #'
 #'         \code{mo.method} a string value indicating the method used for multi-omics integrative clustering.
 #' @import NEMO
-#' @importFrom SNFtool affinityMatrix dist2
 #' @export
 #' @references Rappoport N, Shamir R (2019). NEMO: cancer subtyping by integration of partial multi-omic data. Bioinformatics, 35(18):3348-3356.
 #' @examples # There is no example and please refer to vignette.
--- README.md
+++ README.md
@@ -12,12 +12,11 @@ Multi-Omics integration and VIsualization in Cancer Subtyping
 The goal of MOVICS is to provide a unified interface for 10
 state-of-the-art multi-omics clustering algorithms, and standardizes the
 output for each algorithm so as to form a pipeline for downstream
-analyses. For cancer subtyping studies, MOVICS forms a pipeline for most
-commonly used downstream analyses in cancer subtyping researches and to
-create feature rich customizable visualzations with minimal effort.
+analyses. MOVICS incorporates the most
+commonly used downstream analyses in cancer subtyping researches and enables
+creating feature rich customizable visualzations with minimal effort.
 
 ## Installation
-
 ``` {r}
 if (!require("remotes")) {
   install.packages("remotes")
@@ -28,8 +27,12 @@ remotes::install_github("xlucpu/MOVICS")
 ## Guidance
 
 A detailed guide of how to use MOVICS could be find in the HTML
-vignette. If you have any questions, bug reports, or suggestions for
-improving MOVICS, please email them to <xlu.cpu@foxmail.com>.
+vignette by typing the following code to R session.
+```{r}
+browseVignettes("MOVICS")
+```
+Please email to <xlu.cpu@foxmail.com> if you have any questions, bug reports, or suggestions for
+improving MOVICS. 
 
 ## Citation
 
@@ -39,10 +42,10 @@ If you use MOVICS R package in published research, please cite:
 
 ## Acknowledgement
 
-I would like to express my gratitude to Dr. Morgane Pierre-Jean for the
+I would like to express my gratitude to *Dr. Morgane Pierre-Jean* for the
 inspiration brought by the study of evaluating unsupervised methods for
-multi-omics data integration. I also want to thank Dr. Enyu Lin for the
+multi-omics data integration. I also want to thank *Dr. Enyu Lin* for the
 helping in calculation and visualization of fraction genome altered, and
-to thank Dr. Rongfang Shen for the assistance in visualization of
+to thank *Dr. Rongfang Shen* for the assistance in visualization of
 Transitions and Transversions. At last, sincere thanks to the brilliant
 contributors of all the functions incorporated in MOVICS package.
